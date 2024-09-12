6e980e29952b54349d8827ca831b3d4af9f0659a
Kike Rodriguez Vela <enriquetecfan@Kikes-MacBook-Pro.local>
2024-09-12 10:49:00 +0200

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind(), icon()],
+  site: "https://resumiaweb.vercel.app/",
 });
