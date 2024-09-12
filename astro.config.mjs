a088d33bf8ab75b81b5a32ebd162ce1174745925
Kike Rodriguez Vela <enriquetecfan@Kikes-MacBook-Pro.local>
2024-09-12 10:52:05 +0200

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
-  integrations: [tailwind(), icon()],
   site: "https://resumiaweb.vercel.app/",
+  integrations: [tailwind(), icon()],
 });
